427faf772052f033b9030167f79f04d0094de837
NiAlvarez <nicofacu2014@gmail.com>
2021-03-25 20:42:44 -0300

    Se agregan test unitarios del snapshot de PrimeraApp.js como practica de Jest y Enzyme

---
+++ src/tests/PrimeraApp.test.js
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react"
+import React from 'react';
+import PrimeraApp from "../PrimeraApp"
+import {shallow} from 'enzyme'
+
+describe('Pruebas en <PrimeraApp />', () => {
+
+    // test('Debe de mostrar el mensaje "Hola, soy Goku"',()=>{
+
+    //     const saludo = 'Hola, soy Goku'
+
+    //     const { getByText } = render(<PrimeraApp saludo={saludo} />)
+
+    //     expect(getByText(saludo)).toBeInTheDocument;
+
+    // })
+
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
+
+        const saludo = 'Hola, soy Goku'
+        const wrapper = shallow(<PrimeraApp saludo={saludo} />)
+
+        expect(wrapper).toMatchSnapshot();
+
+    })
+    
+
+})
